d1712cdf03c8580444177e44541b2bb582b3c9d6
Ryunosuke Takagishi <tttrrryu22219@gmail.com>
2016-12-09 09:15:28 +0000

    csvittan

--- db/fixtures/00015_residence.rb
+++ db/fixtures/00015_residence.rb
@@ -1,11 +1,9 @@
 require 'csv'
 
-csv = CSV.read('db/fixtures/csv/university.csv', headers: true)
+csv = CSV.read('db/fixtures/csv/residence.csv', headers: true)
 csv.each do |row|
-  University.seed do |s|
+  Residence.seed do |s|
     s.id = row['id']
     s.name = row['name']
-    s.country_id = row['country_id']
-    s.language_id = row['language_id']
   end
 end
--- db/fixtures/0010_language.rb
+++ db/fixtures/0010_language.rb
@@ -1,11 +1,9 @@
 require 'csv'
 
-csv = CSV.read('db/fixtures/csv/university.csv', headers: true)
+csv = CSV.read('db/fixtures/csv/language.csv', headers: true)
 csv.each do |row|
-  University.seed do |s|
+  Language.seed do |s|
     s.id = row['id']
     s.name = row['name']
-    s.country_id = row['country_id']
-    s.language_id = row['language_id']
   end
 end
